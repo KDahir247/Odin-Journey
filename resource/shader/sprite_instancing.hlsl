00c10fc3dad4f7ef14d985a00796635a682b1724
Khalid <k.dahir243@hotmail.com>
2023-07-23 00:44:43 -0500

    small shader fix

--- resource/shader/sprite_instancing.hlsl
+++ resource/shader/sprite_instancing.hlsl
@@ -44,26 +44,19 @@ struct VsOut{
     float2 uv : UV;
 };
 
-//TODO use inputlayout x.x
 Texture2D<float4> SpriteTexture : register(t0);
 SamplerState SpriteSampler : register (s0);
 
 VsOut vs_main(in VSIn vs_in)
 {
     VsOut vso;
-
-    // float2 position_screen_space = float4(vs_in.position * src_rect.zw, 0.0, 1.0)
-    // position_screen_space = mul(position_screen_space, mvp_matrix)
-
-    // float2 position_device_space = position_screen_space / viewport_size
-    // position_device_space *= device_conversion - float2(1.0, -1.0)
     
-    float4 scaled_quad = float4(vs_in.quadid * vs_in.src_rect.wz, 0.0, 1.0);
+    float4 scaled_quad = float4(vs_in.quadid * vs_in.src_rect.zw, 0.0, 1.0);
 
-    float2 device_conversion = float2(2.0, -2.0) / viewportSize;
-    float2 position = mul(scaled_quad, vs_in.transform).xy * device_conversion * 2  - float2(1.0, -1.0) ;
+    float2 device_conversion = float2(4.0, -4.0) / viewportSize;
+    float2 position = mul(scaled_quad, vs_in.transform).xy * device_conversion - float2(1.0, -1.0);
 
-    vso.position = float4(position.x, position.y, 0.0f,1.0f) ;
+    vso.position = float4(position.x, position.y, 0.0f,1.0f);
     vso.uv = scaled_quad.xy + vs_in.src_rect.xy;
     vso.color = vs_in.color;
 
@@ -71,7 +64,7 @@ VsOut vs_main(in VSIn vs_in)
 }
 
 float4 ps_main(in VsOut vs_out) : SV_TARGET{
-    float2 target_uv = vs_out.uv  * float2(1.0 / 663, 1.0 / 400); //+ ( float2(39, 0) * float2(1.0 / 663, 1.0 / 400)); 
+    float2 target_uv = vs_out.uv  * float2(1.0 / 663, 1.0 / 400); //TODO: khal don't hardcode the rcp sprite sheet size 
     float4 color = SpriteTexture.Sample(SpriteSampler, target_uv);
     return color;
 
